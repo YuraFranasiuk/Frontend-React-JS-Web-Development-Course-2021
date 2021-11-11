ff7495de9bea73edb8293197dc8c85fb9e9b46f2
Yura Franasiuk <yura.franasiuk@gmail.com>
2021-11-11 21:00:57 +0200

    Code refactoring & Bug fix: fixed grammatical case word - "слово" in output

--- Section 2: Basic JS/Practice: Fulfil missing operators in equations and expressions/Task 4/script.js
+++ Section 2: Basic JS/Practice: Fulfil missing operators in equations and expressions/Task 4/script.js
@@ -1,37 +1,26 @@
 function calculateWordsInString(string) {
-    let wordCounter = 0;
+    let wordCount = 0;
+    let wordFound = false;
 
-    let iteratorIsInsideWord = false;
-
-    for (const iterator of string) {
-        if (iterator !== ' ' && !iteratorIsInsideWord) {
-            iteratorIsInsideWord = true;
-
-            wordCounter++;
-        } else if (iterator === ' ') {
-            iteratorIsInsideWord = false;
+    if (typeof string == 'string') {
+        for (const iterator of string) {
+            if (iterator != ' ' && !wordFound) { wordFound = true; wordCount++; }
+            else if (iterator == ' ')          { wordFound = false;             }
         }
     }
 
-    let grammaticalCaseOfWord;
+    let grammaticalCase;
 
-    const wordCounterMod10 = wordCounter % 10;
+    const wordCountMod10  = wordCount % 10;
+    const wordCountMod100 = wordCount % 100;
 
-    switch ((wordCounterMod10 >= 1 && wordCounterMod10 <= 4) ? wordCounterMod10 : wordCounter) {
-        case 1:
-            grammaticalCaseOfWord = 'слово';
-            break;
-        case 2:
-        case 3:
-        case 4:
-            grammaticalCaseOfWord = 'слова';
-            break;
-        default:
-            grammaticalCaseOfWord = 'слів';
-            break;
-    }
+    if (wordCountMod10  == 0                          ||
+        wordCountMod10  >= 5  && wordCountMod10  <= 9 ||
+        wordCountMod100 >= 11 && wordCountMod100 <= 19)  { grammaticalCase = 'слів';  }
+    else if (wordCountMod10 == 1)                        { grammaticalCase = 'слово'; }
+    else if (wordCountMod10 >= 2 && wordCountMod10 <= 4) { grammaticalCase = 'слова'; }
 
-    console.log('"' + string + '"' + ' - ' + wordCounter + ' ' + grammaticalCaseOfWord);
+    console.log('"' + string + '"' + ' - ' + wordCount + ' ' + grammaticalCase);
 }
 
 calculateWordsInString("Easy string for count");
